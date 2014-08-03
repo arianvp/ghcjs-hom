ebf03fcb295d56158e7f61665abe2439a67c0d01
Arian van Putten <aeroboy94@gmail.com>
2014-08-04 01:19:34 +0200

    Add example of App API

---
+++ example/WithElmLikeAPI.hs
@@ -0,0 +1,46 @@
+{-#LANGUAGE TemplateHaskell, OverloadedStrings #-}
+import Hom.App
+import Hom.DOM
+import Hom.VDOM
+import Control.Concurrent
+import Control.Monad
+import Data.Text (pack)
+
+import Control.Lens hiding (Action)
+
+
+data AppState = AppState { _counter :: Int} deriving (Show)
+
+makeLenses ''AppState
+
+data Action = NoOp
+            | Increment
+            | Reset deriving (Show)
+
+
+
+step :: Action -> AppState -> AppState
+step NoOp = id
+step Increment = over counter succ
+step Reset = const $ AppState 0
+
+
+render :: AppState -> Node
+render state =
+  node "div" emptyProps
+    [ text . pack . show $ state^.counter]
+
+
+main = do
+  (app,handle) <- runApp NoOp (AppState 0)  step render
+  body <- getBody
+  body `appendChild` app
+
+  -- two "event emitters"
+  forkIO . forever $ do
+    handle Increment
+    threadDelay $ 1000 * 300
+
+  forkIO . forever $ do
+    handle Reset
+    threadDelay $ 1000 * 4000
